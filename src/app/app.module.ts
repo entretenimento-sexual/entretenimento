f17fbbd517f957ca49b7136176e5abd122c5bfb1
Entretenimento Sexual <entretenimentosexual@gmail.com>
2023-07-30 12:47:18 -0300

    Adicionado componentes do header, auth.service e crud firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,56 @@
 // app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Módulos do seu projeto
+import { HomeModule } from './home/home.module';
+import { PublicContentModule } from './public-content/public-content.module';
+import { ExplorationModule } from './exploration/exploration.module';
+import { CommunityModule } from './community/community.module';
+import { ContactSupportModule } from './contact-support/contact-support.module';
+
+// Serviços do seu projeto
+import { AuthService } from './core/services/autentication/auth.service';
 
 // Firebase
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { environment } from '../environments/environment';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './core/header/logo/logo.component';
+import { NavbarComponent } from './core/header/navbar/navbar.component';
+import { SearchComponent } from './core/header/search/search.component';
+import { UserIconComponent } from './core/header/user-icon/user-icon.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    NavbarComponent,
+    SearchComponent,
+    UserIconComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HomeModule,
+    PublicContentModule,
+    ExplorationModule,
+    CommunityModule,
+    ContactSupportModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
-
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/services/autentication/auth.service.ts
@@ -0,0 +1,34 @@
+// core/services/authentication/auth.service.ts
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: AngularFireAuth) { }
+
+  signup(email: string, password: string) {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  login(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  googleLogin() {
+    const provider = new GoogleAuthProvider();
+    return this.auth.signInWithPopup(provider);
+  }
+
+  facebookLogin() {
+    const provider = new FacebookAuthProvider();
+    return this.auth.signInWithPopup(provider);
+  }
+}
