8355d94a84804e163f31c78d4dd1e98d0f04c522
Entretenimento Sexual <entretenimentosexual@gmail.com>
2024-03-06 20:55:15 -0300

    atualização de pacotes incluindo ng-bootstrap 16.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 // src\main.ts
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
