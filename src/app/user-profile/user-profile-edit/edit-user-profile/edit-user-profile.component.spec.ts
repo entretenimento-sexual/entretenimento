0862253c7f30d8e6975f1aae2efb694d56365098
Alex Seves <alexseves@gmail.com>
2026-06-07 12:31:10 -0300

    style(media): refine photo viewer comments layout

--- src/app/user-profile/user-profile-edit/edit-user-profile/edit-user-profile.component.spec.ts
+++ src/app/user-profile/user-profile-edit/edit-user-profile/edit-user-profile.component.spec.ts
@@ -1,6 +1,8 @@
+//src\app\user-profile\user-profile-edit\edit-user-profile\edit-user-profile.component.spec.ts
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditUserProfileComponent } from './edit-user-profile.component';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('EditUserProfileComponent', () => {
   let component: EditUserProfileComponent;
