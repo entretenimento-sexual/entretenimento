52ed4b2867257dbc1e5a0847e8349eabedbee2ff
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-06-17 19:40:14 -0300

    Compatibiliza modulo do editor standalone

--- src/app/photo-editor/photo-editor.module.ts
+++ src/app/photo-editor/photo-editor.module.ts
@@ -1,19 +1,11 @@
 // src/app/photo-editor/photo-editor.module.ts
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { FormsModule } from '@angular/forms';
 import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
 import { StorageService } from '../core/services/image-handling/storage.service';
 
 @NgModule({
-  declarations: [PhotoEditorComponent], // Adicionado aqui
-  imports: [
-    CommonModule,
-    FormsModule,
-    MatProgressSpinnerModule
-  ],
+  imports: [PhotoEditorComponent],
   providers: [StorageService],
-  exports: [PhotoEditorComponent] // Exporte o componente se necessário
+  exports: [PhotoEditorComponent],
 })
-export class PhotoEditorModule { }
+export class PhotoEditorModule {}
