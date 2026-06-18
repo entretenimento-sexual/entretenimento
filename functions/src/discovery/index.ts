20642dd05a01161ab42d7f7d84dc27962e29d89b
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-06-18 09:45:23 -0300

    Exporta callables de discovery

---
+++ functions/src/discovery/index.ts
@@ -0,0 +1,8 @@
+// functions/src/discovery/index.ts
+// -----------------------------------------------------------------------------
+// DISCOVERY DOMAIN EXPORTS
+// -----------------------------------------------------------------------------
+export {
+  publishUserIntentStatus,
+  hideUserIntentStatus,
+} from './user-intent-status.handler';
