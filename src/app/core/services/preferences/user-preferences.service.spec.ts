5071b8dfd688755ce8adcd41e7696c9d934c9134
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-05 11:32:42 -0300

    test(preferences): mock service dependencies

--- src/app/core/services/preferences/user-preferences.service.spec.ts
+++ src/app/core/services/preferences/user-preferences.service.spec.ts
@@ -1,12 +1,61 @@
 import { TestBed } from '@angular/core/testing';
+import { Firestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
 
 import { UserPreferencesService } from './user-preferences.service';
+import { CacheService } from '../general/cache/cache.service';
+import { FirestoreContextService } from '../data-handling/firestore/core/firestore-context.service';
+import { GlobalErrorHandlerService } from '../error-handler/global-error-handler.service';
+import { ErrorNotificationService } from '../error-handler/error-notification.service';
+import {
+  createStoreTestingMock,
+  provideStoreTestingMock,
+} from '../../../../test/ngrx-store-testing.providers';
 
 describe('UserPreferencesService', () => {
   let service: UserPreferencesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const storeMock = createStoreTestingMock();
+
+    TestBed.configureTestingModule({
+      providers: [
+        ...provideStoreTestingMock(storeMock),
+        {
+          provide: Firestore,
+          useValue: {},
+        },
+        {
+          provide: CacheService,
+          useValue: {
+            get: () => of(null),
+            set: () => undefined,
+          },
+        },
+        {
+          provide: FirestoreContextService,
+          useValue: {
+            deferPromise$: () => of({ docs: [] }),
+            run: async (task: () => Promise<void>) => task(),
+          },
+        },
+        {
+          provide: GlobalErrorHandlerService,
+          useValue: {
+            handleError: () => undefined,
+          },
+        },
+        {
+          provide: ErrorNotificationService,
+          useValue: {
+            showError: () => undefined,
+            showSuccess: () => undefined,
+            showWarning: () => undefined,
+            showInfo: () => undefined,
+          },
+        },
+      ],
+    });
     service = TestBed.inject(UserPreferencesService);
   });
 
