d83222396b31135d91aa65ac04d1515186d10795
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-05 11:15:58 -0300

    test(filter): mock region service dependencies

--- src/app/core/services/filtering/filters/region-filter.service.spec.ts
+++ src/app/core/services/filtering/filters/region-filter.service.spec.ts
@@ -1,12 +1,49 @@
 import { TestBed } from '@angular/core/testing';
+import { Firestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
 
 import { RegionFilterService } from './region-filter.service';
+import { FirestoreContextService } from '../../data-handling/firestore/core/firestore-context.service';
+import { GlobalErrorHandlerService } from '../../error-handler/global-error-handler.service';
+import { ErrorNotificationService } from '../../error-handler/error-notification.service';
+import { IBGELocationService } from '../../general/api/ibge-location.service';
 
 describe('RegionFilterService', () => {
   let service: RegionFilterService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Firestore,
+          useValue: {},
+        },
+        {
+          provide: FirestoreContextService,
+          useValue: {
+            deferPromise$: () => of(null),
+          },
+        },
+        {
+          provide: IBGELocationService,
+          useValue: {
+            getMunicipios: () => of([]),
+          },
+        },
+        {
+          provide: GlobalErrorHandlerService,
+          useValue: {
+            handleError: () => undefined,
+          },
+        },
+        {
+          provide: ErrorNotificationService,
+          useValue: {
+            showError: () => undefined,
+          },
+        },
+      ],
+    });
     service = TestBed.inject(RegionFilterService);
   });
 
