bfa4bbe99859a4ab24417f8cc34ccb7babdf6dda
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-05 15:56:11 -0300

    test(subscription): mock service dependencies

--- src/app/core/services/subscriptions/subscription.service.spec.ts
+++ src/app/core/services/subscriptions/subscription.service.spec.ts
@@ -1,12 +1,45 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { vi } from 'vitest';
 
 import { SubscriptionService } from './subscription.service';
+import { CurrentUserStoreService } from '../autentication/auth/current-user-store.service';
+import { DateTimeService } from '../general/date-time.service';
 
 describe('SubscriptionService', () => {
   let service: SubscriptionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: CurrentUserStoreService,
+          useValue: {
+            user$: of(null),
+          },
+        },
+        {
+          provide: DateTimeService,
+          useValue: {
+            convertToDate: (value: unknown) => new Date(value as string | number | Date),
+          },
+        },
+        {
+          provide: MatDialog,
+          useValue: {
+            open: vi.fn(() => ({ afterClosed: () => of(false) })),
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: vi.fn(),
+          },
+        },
+      ],
+    });
     service = TestBed.inject(SubscriptionService);
   });
 
