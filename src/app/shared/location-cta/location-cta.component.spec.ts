38ae55ac4edfb1b926899fa0a566697e71319e1b
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-05 22:53:58 -0300

    test(location): provide firestore token in cta spec

--- src/app/shared/location-cta/location-cta.component.spec.ts
+++ src/app/shared/location-cta/location-cta.component.spec.ts
@@ -1,6 +1,7 @@
 // src/app/shared/location-cta/location-cta.component.spec.ts
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Firestore } from '@angular/fire/firestore';
 
 import { LocationCtaComponent } from './location-cta.component';
 
@@ -10,7 +11,10 @@ describe('LocationCtaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LocationCtaComponent, RouterTestingModule] // 👈 adiciona
+      imports: [LocationCtaComponent, RouterTestingModule],
+      providers: [
+        { provide: Firestore, useValue: {} },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LocationCtaComponent);
