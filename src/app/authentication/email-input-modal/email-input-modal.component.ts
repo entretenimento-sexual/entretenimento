4adbb4077f66afebaa3e7f4ec9be760bc242dc67
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-08 15:50:53 -0300

    feat(auth): make password recovery modal reactive

--- src/app/authentication/email-input-modal/email-input-modal.component.ts
+++ src/app/authentication/email-input-modal/email-input-modal.component.ts
@@ -1,38 +1,31 @@
-//src\app\authentication\email-input-modal\email-input-modal.component.ts
-import { Component, OnInit } from '@angular/core';
+// src/app/authentication/email-input-modal/email-input-modal.component.ts
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { EmailInputModalService } from 'src/app/core/services/autentication/email-input-modal.service';
 
 @Component({
-    selector: 'app-email-input-modal',
-    templateUrl: './email-input-modal.component.html',
-    styleUrls: ['./email-input-modal.component.css'],
-    standalone: true, 
-    imports: [FormsModule],
+  selector: 'app-email-input-modal',
+  templateUrl: './email-input-modal.component.html',
+  styleUrls: ['./email-input-modal.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [AsyncPipe, FormsModule],
 })
-export class EmailInputModalComponent implements OnInit {
-  public email: string = '';
-  public message: string = '';
-  public isModalOpen: boolean = false;
+export class EmailInputModalComponent {
+  readonly vm$ = this.emailInputModalService.state$;
 
-  constructor(private emailInputModalService: EmailInputModalService){ }
+  constructor(private readonly emailInputModalService: EmailInputModalService) {}
 
-  ngOnInit(): void {
-    this.emailInputModalService.isModalOpen.subscribe(isOpen => {
-      this.isModalOpen = isOpen;
-    });
-
-    this.emailInputModalService.emailSentMessage.subscribe(message => {
-      this.message = message;
-    });
+  updateEmail(email: string): void {
+    this.emailInputModalService.updateEmail(email);
   }
 
-  // Envia o e-mail de recuperação de senha
-  sendEmail(): void {
-    this.emailInputModalService.sendPasswordRecoveryEmail(this.email);
+  sendEmail(email: string): void {
+    this.emailInputModalService.sendPasswordRecoveryEmail(email);
   }
 
-  // Fecha o modal
   closeModal(): void {
     this.emailInputModalService.closeModal();
   }
