9595a9cd0d2751ab3afad95f0e60c4a43307e6f2
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-12 17:29:38 -0300

    feat(media): export public video functions

--- functions/src/media/index.ts
+++ functions/src/media/index.ts
@@ -13,6 +13,11 @@ export {
 } from './application/manage-photo-publication.handler';
 
 export {
+  publishVideo,
+  unpublishVideo,
+} from './application/manage-video-publication.handler';
+
+export {
   cleanupPendingPhotoDeletions,
   deleteProfilePhoto,
 } from './application/delete-profile-photo.handler';
@@ -22,7 +27,15 @@ export {
 } from './application/cleanup-published-photo-assets.handler';
 
 export {
+  cleanupPendingPublishedVideoAssets,
+} from './application/cleanup-published-video-assets.handler';
+
+export {
   getPublicPhotoAccessUrls,
 } from './application/get-public-photo-access-urls.handler';
+
+export {
+  getPublicVideoAccessUrls,
+} from './application/get-public-video-access-urls.handler';
 
 export { recordPhotoView } from './application/record-photo-view.handler';
