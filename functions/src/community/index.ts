c3f2c2608452dd42f71e9c11c484770d161ba58d
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-18 18:35:01 -0300

    feat(communities): export community feed callable

--- functions/src/community/index.ts
+++ functions/src/community/index.ts
@@ -1,3 +1,4 @@
 // functions/src/community/index.ts
 export { getCommunityDiscoveryPage } from './get-community-discovery-page.handler';
+export { getCommunityFeedPage } from './get-community-feed-page.handler';
 export { getCommunityPreview } from './get-community-preview.handler';
