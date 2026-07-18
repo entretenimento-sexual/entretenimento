5f0fd31337aa00e3f00dce6041cded4e305f175e
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-18 17:52:32 -0300

    feat(communities): export preview callables

---
+++ functions/src/community/index.ts
@@ -0,0 +1,3 @@
+// functions/src/community/index.ts
+export { getCommunityDiscoveryPage } from './get-community-discovery-page.handler';
+export { getCommunityPreview } from './get-community-preview.handler';
