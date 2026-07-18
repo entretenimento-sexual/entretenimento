274f00eb94d62e39324474fa088a9976e32943d4
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-18 14:47:00 -0300

    feat(subscribers): export exclusive connections callable

---
+++ functions/src/subscriber-experiences/index.ts
@@ -0,0 +1,8 @@
+// functions/src/subscriber-experiences/index.ts
+// -----------------------------------------------------------------------------
+// SUBSCRIBER EXPERIENCES EXPORTS
+// -----------------------------------------------------------------------------
+
+export {
+  getMyExclusiveConnectionsPage,
+} from './exclusive-connections/get-my-exclusive-connections.handler';
