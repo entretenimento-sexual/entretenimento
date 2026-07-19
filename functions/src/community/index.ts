1594521c9531cf464f38486f87c0e09154fa40c2
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-19 09:27:33 -0300

    feat(community): export creation and private index functions

--- functions/src/community/index.ts
+++ functions/src/community/index.ts
@@ -1,7 +1,9 @@
 // functions/src/community/index.ts
+export { createCommunity } from './create-community.handler';
 export { createVenueCommunity } from './create-venue-community.handler';
 export { getCommunityDiscoveryPage } from './get-community-discovery-page.handler';
 export { getCommunityFeedPage } from './get-community-feed-page.handler';
+export { getMyCommunitiesPage } from './get-my-communities-page.handler';
 export {
   getCommunityMembershipRequests,
   leaveCommunityMembership,
@@ -9,3 +11,4 @@ export {
 } from './community-membership-management.handler';
 export { getCommunityPreview } from './get-community-preview.handler';
 export { requestCommunityMembership } from './request-community-membership.handler';
+export { syncCommunityUserIndex } from './sync-community-user-index.trigger';
