dd2be2d95bf9c156b5084601a50f00e2f865f94a
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-07-26 08:41:23 -0300

    perf(dashboard): register discovery effect lazily

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,14 @@
-//src\app\dashboard\dashboard.module.ts
-import { NgModule } from '@angular/core';
+// src/app/dashboard/dashboard.module.ts
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
+import { DASHBOARD_FEATURE_EFFECTS } from './dashboard-feature.effects';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { FeaturedProfilesComponent } from './featured-profiles/featured-profiles.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,7 @@ import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.co
     DashboardRoutingModule,
     RouterModule,
     NgbModule,
-
-  ]
+    EffectsModule.forFeature(DASHBOARD_FEATURE_EFFECTS),
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
