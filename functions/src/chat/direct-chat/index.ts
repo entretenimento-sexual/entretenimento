71e7608a141bc13dc25ac2362eced1a998c319ef
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-08-01 15:21:28 -0300

    feat(chat): export video reference sender

--- functions/src/chat/direct-chat/index.ts
+++ functions/src/chat/direct-chat/index.ts
@@ -4,4 +4,7 @@
 // -------------------------------------------------------------------
 export { ensureDirectChat } from './application/ensure-direct-chat.handler';
 export { sendDirectMessage } from './application/send-direct-message.handler';
+export {
+  sendDirectVideoReference,
+} from './application/send-direct-video-reference.handler';
 export { deleteDirectMessage } from './application/delete-direct-message.handler';
