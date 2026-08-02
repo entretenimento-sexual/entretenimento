02475a4c9f588cd9317ae8b6e00537f6da201693
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-08-02 00:05:25 -0300

    feat(chat): export protected direct video sharing

--- functions/src/chat/direct-chat/index.ts
+++ functions/src/chat/direct-chat/index.ts
@@ -6,5 +6,5 @@ export { ensureDirectChat } from './application/ensure-direct-chat.handler';
 export { sendDirectMessage } from './application/send-direct-message.handler';
 export {
   sendDirectVideoReference,
-} from './application/send-direct-video-reference.handler';
+} from './application/protected-direct-video-share.handler';
 export { deleteDirectMessage } from './application/delete-direct-message.handler';
