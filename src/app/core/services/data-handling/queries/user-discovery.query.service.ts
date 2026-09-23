a1477c237c721906e3e5a5bffc86821ace7da2c6
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-09-22 21:24:43 -0300

    security: filter expired discovery profiles

--- src/app/core/services/data-handling/queries/user-discovery.query.service.ts
+++ src/app/core/services/data-handling/queries/user-discovery.query.service.ts
@@ -264,6 +264,7 @@ export class UserDiscoveryQueryService {
             UserDiscoveryQueryService.DISCOVERY_COL,
             [
               where('ageEligibilityVerifiedAdult', '==', true),
+      where('ageEligibilityValidUntil', '>', new Date()),
               ...(constraints ?? []),
             ],
             {
