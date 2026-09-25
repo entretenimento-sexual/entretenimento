68696703bb0e71b9bb13d6698fe8d44ff1612703
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-09-25 19:41:20 -0300

    refactor(store): remove retired terms and invite state

--- src/app/store/states/app.state.ts
+++ src/app/store/states/app.state.ts
@@ -1,11 +1,9 @@
 // src/app/store/states/app.state.ts
 import { authReducer } from '../reducers/reducers.user/auth.reducer';
 import { userReducer } from '../reducers/reducers.user/user.reducer';
-import { termsReducer } from '../reducers/reducers.user/terms.reducer';
 import { fileReducer } from '../reducers/reducers.user/file.reducer';
 import { userPreferencesReducer } from '../reducers/reducers.user/user-preferences.reducer';
 
-import { inviteReducer } from '../reducers/reducers.chat/invite.reducer';
 
 import { locationReducer } from '../reducers/reducers.location/location.reducer';
 import { nearbyProfilesReducer } from '../reducers/reducers.location/nearby-profiles.reducer';
@@ -19,13 +17,9 @@ export interface AppState {
   // USER DOMAIN
   auth: ReturnType<typeof authReducer>;
   user: ReturnType<typeof userReducer>;
-  terms: ReturnType<typeof termsReducer>;
   file: ReturnType<typeof fileReducer>;
   userPreferences: ReturnType<typeof userPreferencesReducer>;
   friendsPages: ReturnType<typeof friendsPaginationReducer>;
-
-  // MESSAGING GLOBAL
-  invite: ReturnType<typeof inviteReducer>;
 
   // LOCATION DOMAIN
   location: ReturnType<typeof locationReducer>;
