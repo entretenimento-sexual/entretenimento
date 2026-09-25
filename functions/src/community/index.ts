01b7eb11bef6d4e8e5a952418b0b0fbf95381d32
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-09-25 11:06:40 -0300

    feat(community): export internal search callable

--- functions/src/community/index.ts
+++ functions/src/community/index.ts
@@ -77,6 +77,7 @@ export {
   reviewCommunityFeedCommentReplyReport,
 } from './review-community-feed-comment-reply-report.handler';
 export { getCommunityTopicsPage } from './get-community-topics-page.handler';
+export { getCommunitySearchPage } from './get-community-search-page.handler';
 export {
   getCommunityTopicDetail,
   getCommunityTopicRepliesPage,
