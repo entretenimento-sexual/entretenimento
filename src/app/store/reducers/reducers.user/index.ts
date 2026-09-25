c880e851da0c83bc6fbdf4a0f1baac446533e8be
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-09-25 19:41:52 -0300

    refactor(store): remove retired terms reducer wiring

--- src/app/store/reducers/reducers.user/index.ts
+++ src/app/store/reducers/reducers.user/index.ts
@@ -2,14 +2,12 @@
 // Não esqueça os comentários
 import { authReducer } from './auth.reducer';
 import { userReducer } from './user.reducer';
-import { termsReducer } from './terms.reducer';
 import { fileReducer } from './file.reducer';
 import { userPreferencesReducer } from './user-preferences.reducer';
 
 export const userReducers = {
   auth: authReducer,
   user: userReducer,
-  terms: termsReducer,
   file: fileReducer,
   userPreferences: userPreferencesReducer,
 };
