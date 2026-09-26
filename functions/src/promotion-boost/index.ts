f347ada124196f557c1dc7f9b53652e44a4d1dea
entretenimento-sexual <139718528+entretenimento-sexual@users.noreply.github.com>
2026-09-26 15:13:39 -0300

    feat(promotion): export photo promotion backend

---
+++ functions/src/promotion-boost/index.ts
@@ -0,0 +1,5 @@
+export { managePhotoPromotionCampaign } from './manage-photo-promotion-campaign.handler';
+export { getPhotoPromotionPlacement } from './get-photo-promotion-placement.handler';
+export { recordPhotoPromotionEvent } from './record-photo-promotion-event.handler';
+export { getPhotoPromotionCampaignDashboard } from './get-photo-promotion-campaign-dashboard.handler';
+export { runPromotionBoostLifecycle } from './run-promotion-boost-lifecycle.schedule';
